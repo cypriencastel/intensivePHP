7706ad9d2d06239ce891a4e5885633f650f11a9e
« <cyprien.castel@hetic.net>
2016-04-11 12:03:39 +0200

    start

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'src/includes/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<title>Template 04</title>
+	<link rel="stylesheet" href="src/css/reset.css">
+	<link rel="stylesheet" href="src/css/style.css">
+</head>
+<body>
+	<?php echo "hello world"; ?>
+<script src="src/js/libs/jquery-2.2.0.min.js"></script>
+<script src="src/js/app/main.js"></script>
+</body>
+</html>
